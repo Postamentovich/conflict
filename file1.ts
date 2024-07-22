dd4ec1afa6ada0a135795f9ebf1b4adc3a32658f
Viacheslav Zinovev <vdzin@yandex-team.ru>
2024-07-22 13:27:14 +0300

    a

--- file1.ts
+++ file1.ts
@@ -24,6 +24,7 @@ export const AlertActions = ({items, children, className}: AlertActionsProps) =>
                     {text}
                 </AlertAction>
             )) || children}
+            123
         </Flex>
     );
 };
